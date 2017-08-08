9561192eb8f7518d1cb2296942b029cb32c01610
niranjankala <niranjankala@aol.com>
2017-08-08 12:51:34 +0530

    -

---
+++ DiagramControl/ItemType.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagramControl
+{
+    public enum ItemType
+    {
+        HVACAir,
+        ZoneHVACGroup,
+        SHWGroup,
+        HVACHotWater,
+        HVACChilledWater,
+        HVACCondenser,
+        HVACMixedWater,
+        HVACServiceHotWater
+
+    }
+}
