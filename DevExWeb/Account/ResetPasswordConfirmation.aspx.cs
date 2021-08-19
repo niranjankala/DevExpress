215cd4751aaf9db0510bd9c835d796d961ccba35
Niranjan Singh <niranjan.kala@gmail.com>
2021-08-19 21:22:13 +0530

    -

--- DevExWeb/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace DevExWeb.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- WindowsFormsApplication1/XtraReport1.cs
+++
@@ -1,7 +0,0 @@
-namespace LimitNumberOfRecordsPerPage {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-    }
-}
